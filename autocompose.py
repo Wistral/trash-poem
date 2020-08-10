6428a7c6448a0084a0188d13450863bdb61d3862
Wistral <whistral@gmail.com>
2020-08-10 10:13:35 +0800

    fix reset_schedule

--- autocompose.py
+++ autocompose.py
@@ -14,23 +14,20 @@ def reset_schedule():
         lines = f.readlines()
 
     for i, line in enumerate(lines):
-        pattern = "\s+-\scron:\s'(\d+)\s(\d+)(.*)'"
+        pattern = "(\s+-\scron:\s')(\d+)\s(\d+)(.*)"
         if re.match(pattern, line):
             print('match', line)
-            m, h, _ = re.findall(pattern, line)[0]
+            L, m, h, R = re.findall(pattern, line)[0]
             h = int(h)
             m = int(m)
             current_min = h*60+m
             interval = random.randint(4*60, 12*60)
             next_h, next_m = divmod(current_min+interval, 60)
             next_h %= 24
-            if next_h == next_m:
-                next_m += 1
-                next_m %= 60
             print(f'{h}:{m} + {interval} ==> {next_h}:{next_m}')
-            new_line = line.replace(str(h), str(
-                next_h)).replace(str(m), str(next_m))
-            print(repr(line), '==>', repr(new_line))
+            p = "\s+-\scron:\s'(\d+\s\d+).*"
+            new_line = re.sub(p, f'{L}{next_m} {next_h}{R}', line)
+            print(repr(line), '\t==>\n', repr(new_line), sep='')
             lines[i] = new_line
             break
     with open(action, 'w') as f:
